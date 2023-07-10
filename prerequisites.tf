fbe4bbc95db449282d6ff9bcb6764d215e90751f
Assaf Giladi <assaf@weka.io>
2023-07-10 12:48:35 +0300

    chore: add obs to ec2 iam permissions

--- prerequisites.tf
+++ prerequisites.tf
@@ -20,6 +20,8 @@ module "iam" {
   source = "./modules/iam"
   prefix = var.prefix
   cluster_name = var.cluster_name
+  state_bucket_name = local.state_bucket_name
+  obs_name = var.obs_name
 }
 
 locals {
