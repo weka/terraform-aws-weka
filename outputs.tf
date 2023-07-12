49ee3d4ce6ec3db679841bd77bd6227b3b3b2efd
Assaf Giladi <assaf@weka.io>
2023-07-12 10:15:09 +0300

    chore: add progress status lambda

--- outputs.tf
+++ outputs.tf
@@ -5,23 +5,17 @@ locals {
 
 output "cluster_helpers_commands" {
   value = <<EOT
-############################################## Path to ssh keys  ##########################################################################
+############################################## path to ssh keys  ##########################################################################
 ${local.path_ssh_keys}
 user: ${var.vm_username}
 
 ############################################## ec2 public ips #############################################################################
 aws ec2 describe-instances --instance-ids $(aws autoscaling describe-auto-scaling-groups | jq -r '.AutoScalingGroups[]| select( .Tags[].Value == "${var.cluster_name}").Instances[].InstanceId') | jq -r '.Reservations[].Instances[].PublicIpAddress'
 
-
-############################################## deploy url     #############################################################################
-${aws_lambda_function_url.deploy_lambda_url.function_url}
-
-
-##############################################      state     #############################################################################
-aws s3api get-object --bucket ${local.state_bucket_name} --key state /dev/stdout
+############################################## status url     #############################################################################
+curl ${aws_lambda_function_url.status_lambda_url.function_url} -H "Content-Type:application/json" -d '{"type":"progress"}'
 
 ############################################## cluster password ###########################################################################
 aws secretsmanager get-secret-value --secret-id ${aws_secretsmanager_secret.weka_password.id} --query SecretString --output text
-
 EOT
 }
