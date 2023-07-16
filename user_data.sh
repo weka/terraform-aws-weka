449cfe55cb6d1a75c85d63a88fe37c85ecaffcb9
Assaf Giladi <assaf@weka.io>
2023-07-16 07:37:22 +0300

    fix: handle properly list of security group ids

--- user_data.sh
+++ user_data.sh
@@ -24,12 +24,11 @@ instance_id=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.2
 
 region=${region}
 subnet_id=${subnet_id}
-groups=${groups}
 nics_num=${nics_num}
 
 for (( i=1; i<nics_num; i++ ))
 do
-  eni=$(aws ec2 create-network-interface --region "$region" --subnet-id "$subnet_id" --groups "$groups")
+  eni=$(aws ec2 create-network-interface --region "$region" --subnet-id "$subnet_id" --groups ${groups}) # groups should not be in quotes it needs to be a list
   network_interface_id=$(echo "$eni" | python3 -c "import sys, json; print(json.load(sys.stdin)['NetworkInterface']['NetworkInterfaceId'])")
   attachment=$(aws ec2 attach-network-interface --region "$region" --device-index "$i" --instance-id "$instance_id" --network-interface-id "$network_interface_id")
   attachment_id=$(echo "$attachment" | python3 -c "import sys, json; print(json.load(sys.stdin)['AttachmentId'])")
