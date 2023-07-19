172505de481c83e0da51fc282ea87ff3e2f5dee0
Denise Perez <densie@weka.io>
2023-07-19 12:15:00 +0300

    feat: add step function for scale down lambda

---
+++ modules/iam/event.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "event_iam_role" {
+  name               = "${var.prefix}-${var.cluster_name}-event-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = ["events.amazonaws.com","states.amazonaws.com"]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "event_iam_policy" {
+  name = "${var.prefix}-${var.cluster_name}-event-policy"
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "states:StartExecution"
+        ]
+        Resource = [
+          "arn:aws:states:*:*:stateMachine:${var.prefix}-${var.cluster_name}-scale-down-state-machine"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "event_policy_attachment" {
+  policy_arn = aws_iam_policy.event_iam_policy.arn
+  role       = aws_iam_role.event_iam_role.name
+}
--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -5,3 +5,11 @@ output "instance_iam_profile_arn" {
 output "lambda_iam_role_arn" {
   value = aws_iam_role.lambda_iam_role.arn
 }
+
+output "sfn_iam_role_arn" {
+  value = aws_iam_role.sfn_iam_role.arn
+}
+
+output "event_iam_role_arn" {
+  value = aws_iam_role.event_iam_role.arn
+}
