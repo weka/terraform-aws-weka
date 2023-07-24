06b5ed7bc06ac8193d81376b7ca2bfd0b9173d5c
Assaf Giladi <assaf@weka.io>
2023-07-24 10:30:45 +0300

    chore: improve output structure

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,35 @@
 locals {
-  path_ssh_keys = var.ssh_public_key == null ? "${local.ssh_path}-public-key.pub\n${local.ssh_path}-private-key.pem" : ""
-  ips           = var.private_network ? "PrivateIpAddress" : "PublicIpAddress"
+  ips_type = var.private_network ? "PrivateIpAddress" : "PublicIpAddress"
 }
 
+output "ssh_user" {
+  value = var.vm_username
+}
 
-output "cluster_helpers_commands" {
+output "local_ssh_private_key" {
+  value = var.ssh_public_key == null && var.key_pair_name == null ? "${local.ssh_path}-private-key.pem" : null
+}
+
+output "cluster_name" {
+  value = var.cluster_name
+}
+
+output "ips_type" {
+  value = local.ips_type
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.status_lambda.function_name
+}
+
+output "weka_cluster_password_secret_id" {
+  value = aws_secretsmanager_secret.weka_password.id
+}
+
+output "helper_commands" {
   value = <<EOT
-############################################## path to ssh keys  ##########################################################################
-${local.path_ssh_keys}
-user: ${var.vm_username}
-
-############################################## ec2 public ips #############################################################################
-aws ec2 describe-instances --instance-ids $(aws autoscaling describe-auto-scaling-groups | jq -r '.AutoScalingGroups[]| select( .Tags[].Value == "${var.cluster_name}").Instances[].InstanceId') | jq -r '.Reservations[].Instances[].${local.ips}'
-
-##############################################   status url   #############################################################################
+aws ec2 describe-instances --instance-ids $(aws autoscaling describe-auto-scaling-groups | jq -r '.AutoScalingGroups[]| select( .Tags[].Value == "${var.cluster_name}").Instances[].InstanceId') | jq -r '.Reservations[].Instances[].${local.ips_type}'
 aws lambda invoke --function-name ${aws_lambda_function.status_lambda.function_name} --payload '{"type": "progress"}' --cli-binary-format raw-in-base64-out /dev/stdout
-
-##############################################      state     #############################################################################
-aws dynamodb get-item --table-name ${local.dynamodb_table_name} --key '{"${local.dynamodb_hash_key_name}": {"S": "${local.state_key}"}}' | jq -r '.Item.Value.M'
-
-############################################## cluster password ###########################################################################
 aws secretsmanager get-secret-value --secret-id ${aws_secretsmanager_secret.weka_password.id} --query SecretString --output text
 EOT
 }
