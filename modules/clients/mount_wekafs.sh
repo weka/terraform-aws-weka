f3fccbb5ccbc94c7b3e69ed90cdff8ce01ab920a
Kristina Solovyova <kristina.solovyova@weka.io>
2023-07-31 18:58:52 +0300

    feat: add clients

---
+++ modules/clients/mount_wekafs.sh
@@ -0,0 +1,125 @@
+echo "$(date -u): before weka agent installation"
+
+INSTALLATION_PATH="/tmp/weka"
+mkdir -p $INSTALLATION_PATH
+cd $INSTALLATION_PATH
+
+# if alb_dns_name if not empty string, then use alb_dns_name as ips
+if [ -z "${alb_dns_name}" ]; then
+  # Function to get the private IPs of instances in Auto Scaling Group
+  get_private_ips() {
+    instance_ids=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[?contains(Tags[?Value=='${weka_cluster_name}'].Value, '${weka_cluster_name}')].Instances[].InstanceId" --output text --region ${region})
+    private_ips=$(aws ec2 describe-instances --instance-ids $instance_ids --query "Reservations[].Instances[].PrivateIpAddress" --output text --region ${region})
+    private_ips_array=($private_ips)
+  }
+
+  # Retry until the length of the array is not 'weka_cluster_size'
+  while true; do
+    get_private_ips
+    length=$${#private_ips_array[@]}
+    # if the length == weka_cluster_size , break out of the loop
+    if [ $length -eq ${weka_cluster_size} ]; then
+      break
+    fi
+    # sleep for a while (optional) and retry
+    echo "Waiting for all backend instances to be up... $length/$${weka_cluster_size}"
+    sleep 5
+  done
+
+  ips=$private_ips_array
+else
+  ips=(${alb_dns_name})
+fi
+
+backend_ip="$${ips[RANDOM % $${#ips[@]}]}"
+# install weka using random backend ip from ips list
+function retry_weka_install {
+  retry_max=60
+  retry_sleep=30
+  count=$retry_max
+
+  while [ $count -gt 0 ]; do
+      curl --fail -o install_script.sh $backend_ip:14000/dist/v1/install && break
+      count=$(($count - 1))
+      backend_ip="$${ips[RANDOM % $${#ips[@]}]}"
+      echo "Retrying weka install from $backend_ip in $retry_sleep seconds..."
+      sleep $retry_sleep
+  done
+  [ $count -eq 0 ] && {
+      echo "weka install failed after $retry_max attempts"
+      echo "$(date -u): weka install failed"
+      return 1
+  }
+  chmod +x install_script.sh && ./install_script.sh
+  return 0
+}
+
+retry_weka_install
+
+echo "$(date -u): weka agent installation complete"
+
+FILESYSTEM_NAME=default # replace with a different filesystem at need
+MOUNT_POINT=/mnt/weka # replace with a different mount point at need
+mkdir -p $MOUNT_POINT
+
+weka local stop && weka local rm -f --all
+
+gateways="${all_gateways}"
+NICS_NUM="${nics_num}"
+eth0=$(ifconfig | grep eth0 -C2 | grep 'inet ' | awk '{print $2}')
+
+function getNetStrForDpdk() {
+	i=$1
+	j=$2
+	gateways=$3
+	gateways=($gateways) #azure and gcp
+
+	net="-o net="
+	for ((i; i<$j; i++)); do
+		eth=eth$i
+		subnet_inet=$(ifconfig $eth | grep 'inet ' | awk '{print $2}')
+		if [ -z $subnet_inet ] || [ $${#gateways[@]} -eq 0 ];then
+			net="$net$eth" #aws
+			continue
+		fi
+		enp=$(ls -l /sys/class/net/$eth/ | grep lower | awk -F"_" '{print $2}' | awk '{print $1}') #for azure
+		if [ -z $enp ];then
+			enp=$(ethtool -i $eth | grep bus-info | awk '{print $2}') #pci for gcp
+		fi
+		bits=$(ip -o -f inet addr show $eth | awk '{print $4}')
+		IFS='/' read -ra netmask <<< "$bits"
+		
+		gateway=$${gateways[$i]}
+    net="$net$enp/$subnet_inet/$${netmask[1]}/$gateway"
+	done
+}
+
+function retry {
+  local retry_max=$1
+  local retry_sleep=$2
+  shift 2
+  local count=$retry_max
+  while [ $count -gt 0 ]; do
+      "$@" && break
+      count=$(($count - 1))
+      echo "Retrying $* in $retry_sleep seconds..."
+      sleep $retry_sleep
+  done
+  [ $count -eq 0 ] && {
+      echo "Retry failed [$retry_max]: $*"
+      echo "$(date -u): retry failed"
+      return 1
+  }
+  return 0
+}
+
+mount_command="mount -t wekafs -o net=udp $backend_ip/$FILESYSTEM_NAME $MOUNT_POINT"
+if [[ ${mount_clients_dpdk} == true ]]; then
+  getNetStrForDpdk $(($NICS_NUM-1)) $(($NICS_NUM)) "$gateways"
+  mount_command="mount -t wekafs $net -o num_cores=1 -o mgmt_ip=$eth0 $backend_ip/$FILESYSTEM_NAME $MOUNT_POINT"
+fi
+
+retry 60 45 $mount_command
+
+rm -rf $INSTALLATION_PATH
+echo "$(date -u): wekafs mount complete"
---
+++ modules/clients/variables.tf
@@ -0,0 +1,102 @@
+variable "availability_zone" {
+  type        = string
+  description = "AZ in which all the resources will be deployed"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Id of the subnet"
+}
+
+variable "weka_cluster_size" {
+  type        = number
+  description = "Number of backends in the weka cluster"
+}
+
+variable "weka_cluster_name" {
+  type        = string
+  description = "Name of the weka cluster"
+}
+
+variable "nics" {
+  type        = number
+  default     = 2
+  description = "Number of nics to set on each client vm"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "ami id"
+  default     = null
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The virtual machine type (sku) to deploy"
+}
+
+variable "clients_name" {
+  type        = string
+  description = "The clients name."
+}
+
+variable "clients_number" {
+  type        = number
+  description = "The number of virtual machines to deploy."
+  default     = 2
+}
+
+variable "key_pair_name" {
+  type        = string
+  description = "Ssh key pair name to pass to the instances."
+}
+
+variable "proxy_url" {
+  type = string
+}
+
+variable "mount_clients_dpdk" {
+  type        = bool
+  default     = true
+  description = "Install weka cluster with DPDK"
+}
+
+variable "assign_public_ip" {
+  type        = bool
+  default     = true
+  description = "Determines whether to assign public ip."
+}
+
+variable "sg_ids" {
+  type        = list(string)
+  default     = []
+  description = "Security group ids"
+}
+
+variable "placement_group_name" {
+  type        = string
+  description = "Placement group name"
+  default     = ""
+}
+
+variable "tags_map" {
+  type        = map(string)
+  default     = { "env" : "dev", "creator" : "tf" }
+  description = "A map of tags to assign the same metadata to all resources in the environment. Format: key:value."
+}
+
+variable "instance_iam_profile_arn" {
+  type        = string
+  description = "Instance IAM profile ARN"
+}
+
+variable "root_volume_size" {
+  type        = number
+  description = "The root volume size in GB"
+}
+
+variable "alb_dns_name" {
+  type        = string
+  description = "ALB DNS name"
+  default     = null
+}
