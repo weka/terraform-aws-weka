34d2baded32aaca8a89040c117f931a8cfa68207
Assaf Giladi <assaf@weka.io>
2023-09-21 09:55:27 +0300

    chore: add clients ips to clients module

--- modules/clients/outputs.tf
+++ modules/clients/outputs.tf
@@ -1,3 +1,7 @@
 output "clients_name" {
   value = var.clients_name
 }
+
+output "client_ips" {
+  value = var.clients_number == 0 ? null : var.assign_public_ip ? aws_instance.this.*.public_ip : aws_instance.this.*.private_ip
+}
