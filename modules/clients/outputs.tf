143df13983b59d01bec384a5324e4118bd805f71
Kristina Solovyova <kristina.solovyova@weka.io>
2023-09-27 12:11:25 +0300

    fix: address tflint warnings

--- modules/clients/outputs.tf
+++ modules/clients/outputs.tf
@@ -2,6 +2,16 @@ output "clients_name" {
   value = var.clients_name
 }
 
+locals {
+  public_ips = [
+    for i in range(var.var.clients_number) :
+    aws_instance.this[i].public_ip
+  ]
+  private_ips = [
+    for i in range(var.var.clients_number) :
+    aws_instance.this[i].private_ip
+  ]
+}
 output "client_ips" {
-  value = var.clients_number == 0 ? null : var.assign_public_ip ? aws_instance.this.*.public_ip : aws_instance.this.*.private_ip
+  value = var.clients_number == 0 ? null : var.assign_public_ip ? local.public_ips : local.private_ips
 }
