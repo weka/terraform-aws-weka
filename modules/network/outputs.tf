4d4d6d2aafe7df7c07b6d68e2771e912989ed528
Denise Perez <densie@weka.io>
2023-10-22 15:22:46 +0300

    fix: subnets ids for private network

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,14 +1,24 @@
 locals {
-  subnet_ids       = var.private_network ? aws_subnet.private_subnet[*].id : aws_subnet.public_subnet[*].id
-  subnet_ids_count = length(local.subnet_ids)
+  public_subnet_ids  = aws_subnet.public_subnet[*].id
+  private_subnet_ids = var.private_network ? aws_subnet.private_subnet[*].id : []
+  subnets_ids        = var.private_network ? concat(local.public_subnet_ids, local.private_subnet_ids) : local.public_subnet_ids
+  subnet_ids_count   = length(local.subnets_ids)
+}
+
+output "private_subnets" {
+  value = local.private_subnet_ids
+}
+
+output "public_subnets" {
+  value = local.public_subnet_ids
 }
 
 output "subnet_ids" {
-  value = var.additional_subnet ? slice(local.subnet_ids, 0, local.subnet_ids_count - 1) : local.subnet_ids
+  value = var.additional_subnet ? slice(local.subnets_ids, 0, local.subnet_ids_count - 1) : local.subnets_ids
 }
 
 output "additional_subnet_id" {
-  value = local.subnet_ids[local.subnet_ids_count - 1]
+  value = local.subnets_ids[local.subnet_ids_count - 1]
 }
 
 output "vpc_id" {
