cd0bbe52815495bcf2c5c4380a33686aa224e86c
Assaf Giladi <assaf@weka.io>
2023-12-22 11:38:41 +0200

    core: update default weka version to 4.2.6.90

--- variables.tf
+++ variables.tf
@@ -138,7 +138,7 @@ variable "cluster_name" {
 variable "weka_version" {
   type        = string
   description = "The Weka version to deploy."
-  default     = "4.2.6"
+  default     = "4.2.6.90"
 }
 
 variable "get_weka_io_token" {
