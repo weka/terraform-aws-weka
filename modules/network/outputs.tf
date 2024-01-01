d18563542fe64b46a95a1acea10a10ec3ed982fb
Assaf Giladi <assaf@weka.io>
2024-01-01 22:34:51 +0200

    temp

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,8 +1,10 @@
 locals {
   public_subnet_ids  = aws_subnet.public_subnet[*].id
   private_subnet_ids = var.subnet_autocreate_as_private ? aws_subnet.private_subnet[*].id : []
-  subnets_ids        = var.subnet_autocreate_as_private ? local.private_subnet_ids : local.public_subnet_ids
-  subnet_ids_count   = length(local.subnets_ids)
+  all_subnets_ids        = var.subnet_autocreate_as_private ? local.private_subnet_ids : local.public_subnet_ids
+  subnet_ids_count   = length(local.all_subnets_ids)
+  deployment_subnet_ids = var.additional_subnet ? slice(local.all_subnets_ids, 0, local.subnet_ids_count - 1) : local.all_subnets_ids
+  additional_subnet_id =  var.additional_subnet ? local.all_subnets_ids[local.subnet_ids_count - 1]: null
 }
 
 output "private_subnets" {
@@ -16,12 +18,12 @@ output "public_subnets" {
 }
 
 output "subnet_ids" {
-  value       = var.additional_subnet ? slice(local.subnets_ids, 0, local.subnet_ids_count - 1) : local.subnets_ids
+  value       = coalesce(local.deployment_subnet_ids, [])
   description = "List of subnet ids without the `additional subnet`"
 }
 
 output "additional_subnet_id" {
-  value       = local.subnets_ids[local.subnet_ids_count - 1]
+  value       = local.additional_subnet_id
   description = "Additional subnet id"
 }
 
