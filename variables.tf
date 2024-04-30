b2d16814fc00df1dd1775913b627513aae2863dd
Assaf Giladi <assaf@weka.io>
2024-04-30 21:16:48 +0300

    chore: update weka default version: 4.2.10

--- variables.tf
+++ variables.tf
@@ -158,7 +158,7 @@ variable "cluster_name" {
 variable "weka_version" {
   type        = string
   description = "The Weka version to deploy."
-  default     = "4.2.9.28"
+  default     = "4.2.10"
 }
 
 variable "get_weka_io_token" {
