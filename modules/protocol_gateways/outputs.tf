81de5c2d36293feeae183e90860c0a5d1acdf4fd
Assaf Giladi <assaf@weka.io>
2024-05-21 23:28:46 +0300

    chore: add smb instances destroy command to output

--- modules/protocol_gateways/outputs.tf
+++ modules/protocol_gateways/outputs.tf
@@ -2,3 +2,8 @@ output "gateways_name" {
   value       = var.gateways_name
   description = "Protocol gateway name"
 }
+
+output "instance_ids" {
+  value       = aws_instance.this[*].id
+  description = "Protocol gateway instance ids."
+}
