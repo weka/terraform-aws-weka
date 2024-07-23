55c15bab82bf0d685a54e54fa86726503186a5bd
Denise Perez <densie@weka.io>
2024-07-23 16:28:37 +0300

    fix: acm module

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -18,13 +18,13 @@ resource "aws_acm_certificate" "acm_certificate_request" {
 
 resource "aws_route53_record" "acm_validation_record" {
   for_each = {
-    for dvo in aws_acm_certificate.acm_certificate_request[0].domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.acm_certificate_request.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
   }
-  zone_id         = data.aws_route53_zone.domain[0].id
+  zone_id         = data.aws_route53_zone.domain.id
   name            = each.value.name
   type            = each.value.type
   allow_overwrite = true
@@ -37,6 +37,6 @@ resource "aws_acm_certificate_validation" "acm_certificate_validation" {
   timeouts {
     create = "7m"
   }
-  certificate_arn         = aws_acm_certificate.acm_certificate_request[0].arn
+  certificate_arn         = aws_acm_certificate.acm_certificate_request.arn
   validation_record_fqdns = [for record in aws_route53_record.acm_validation_record : record.fqdn]
 }
