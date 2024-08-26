ba4cb23e4ecb8b6431eb845a75c7441d65a04578
Assaf Giladi <assaf@weka.io>
2024-08-26 19:20:45 +0300

    Revert "feat: support ubuntu 22 for clients"
    This reverts commit fc5c7e5b08cc7d1fe447fd3c35c0000580726270.

--- clients.tf
+++ clients.tf
@@ -15,7 +15,7 @@ module "clients" {
   assign_public_ip             = local.assign_public_ip
   placement_group_name         = var.client_placement_group_name != null || !var.client_use_backends_placement_group ? var.client_placement_group_name : local.backends_placement_group_name
   use_placement_group          = var.use_placement_group
-  root_volume_size             = var.client_root_volume_size
+  weka_volume_size             = var.client_weka_volume_size
   client_instance_ami_id       = var.client_instance_ami_id
   sg_ids                       = local.sg_ids
   tags_map                     = var.tags_map
