69051a9d89758d444f72383660fe7320a018688b
Assaf Giladi <assaf@weka.io>
2024-08-26 19:20:45 +0300

    Revert "feat: support ubuntu 22 for clients"
    This reverts commit fc5c7e5b08cc7d1fe447fd3c35c0000580726270.

--- modules/clients/main.tf
+++ modules/clients/main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "selected" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-${local.arch}-server-202308*"]
+    values = ["amzn2-ami-kernel-5.*-${local.arch}-gp2"]
   }
 }
 
@@ -44,9 +44,8 @@ locals {
   custom_data_parts = [local.preparation_script, local.mount_wekafs_script, "${var.custom_data}\n"]
   custom_data       = base64encode(join("\n", local.custom_data_parts))
   arm_instances     = ["c7gd.2xlarge", "c7gd.4xlarge", "c7gd.8xlarge", "c7gd.12xlarge", "c7gd.16xlarge", "c7g.2xlarge", "c7g.4xlarge", "c7g.8xlarge", "c7g.12xlarge", "c7g.16xlarge", "m7gd.xlarge", "m7gd.2xlarge", "m7gd.4xlarge", "m7gd.8xlarge", "m7gd.12xlarge", "m7gd.16xlarge", "m7g.xlarge", "m7g.2xlarge", "m7g.4xlarge", "m7g.8xlarge", "m7g.12xlarge", "m7g.16xlarge", "c6gn.2xlarge", "c6gn.4xlarge", "c6gn.8xlarge", "c6gn.12xlarge", "c6gn.16xlarge", "c6gd.2xlarge", "c6gd.4xlarge", "c6gd.8xlarge", "c6gd.12xlarge", "c6gd.16xlarge", "c6g.2xlarge", "c6g.4xlarge", "c6g.8xlarge", "c6g.12xlarge", "c6g.16xlarge", "m6gd.xlarge", "m6gd.2xlarge", "m6gd.4xlarge", "m6gd.8xlarge", "m6gd.12xlarge", "m6gd.16xlarge", "m6g.xlarge", "m6g.2xlarge", "m6g.4xlarge", "m6g.8xlarge", "m6g.12xlarge", "m6g.16xlarge", "g5g.2xlarge", "g5g.4xlarge", "g5g.8xlarge", "g5g.16xlarge"]
-  default_arch      = contains(local.arm_instances, var.instance_type) ? "arm64" : "amd64"
+  default_arch      = contains(local.arm_instances, var.instance_type) ? "arm64" : "x86_64"
   arch              = var.arch == null ? local.default_arch : var.arch
-  root_volume_size  = var.client_instance_ami_id == null ? 48 : var.root_volume_size
 }
 
 resource "aws_placement_group" "this" {
@@ -67,9 +66,9 @@ resource "aws_launch_template" "this" {
   update_default_version               = true
 
   block_device_mappings {
-    device_name = "/dev/sda1"
+    device_name = "/dev/xvda"
     ebs {
-      volume_size           = local.root_volume_size
+      volume_size           = var.weka_volume_size
       volume_type           = "gp3"
       delete_on_termination = true
       encrypted             = var.ebs_encrypted
