c22a3b1f1c0454f2caad13015c4d645ec53fae59
Almog Agan <almog.agan@weka.io>
2024-09-23 20:06:53 +0300

    mor debug

--- modules/protocol_gateways/setup_s3.sh
+++ modules/protocol_gateways/setup_s3.sh
@@ -4,23 +4,28 @@ not_ready_hosts=$(weka s3 cluster status | grep 'Not Ready' | wc -l)
 all_hosts=$(weka s3 cluster status | grep 'Host' | wc -l)
 
 function check_cluster_status() {
+  echo "almog enter check_cluster_status "
   if (( all_hosts > 0 && not_ready_hosts == 0 && all_hosts == cluster_size )); then
+          echo "almog enter if "
           echo "$(date -u): s3 cluster is already created"
           weka s3 cluster status
           exit 0
   fi
+  echo "almog exit check_cluster_status "
 }
 
 # get all protocol gateways frontend container ids separated by comma
 all_container_ids_str=$(echo "$all_container_ids" | tr '\n' ',' | sed 's/,$//')
 
 function retry_create_s3_cluster {
+  echo "almog enter retry_create_s3_cluster"
   retry_max=60
   retry_sleep=30
   count=$retry_max
   check_cluster_status
+  echo "almog after check_cluster_status "
   while [ $count -gt 0 ]; do
-      echo "about to run create command with containers $all_container_ids_str"
+      echo "almog about to run create command with containers $all_container_ids_str"
       weka fs
       weka s3 cluster status
       weka s3 cluster create $filesystem_name .config_fs --container $all_container_ids_str --port 9000 && break
@@ -38,7 +43,9 @@ function retry_create_s3_cluster {
 }
 
 if [[ $(weka s3 cluster status) ]]; then
+        echo "almog before check_cluster_status 1 "
         check_cluster_status
+        echo "almog after check_cluster_status 1 "
         if (( all_hosts > 0 && not_ready_hosts == 0 && all_hosts < cluster_size )); then
               echo "$(date -u): S3 cluster already exists, adding current container to it"
               weka s3 cluster containers add $container_id
@@ -48,7 +55,9 @@ if [[ $(weka s3 cluster status) ]]; then
         fi
 else
   echo "$(date -u): weka S3 cluster does not exist, creating it"
+  echo "almog before retry_create_s3_cluster 1 "
   retry_create_s3_cluster
+  echo "almog after retry_create_s3_cluster 1 "
   echo "$(date -u): Successfully create S3 cluster..."
   weka s3 cluster status
   weka s3 cluster containers list
