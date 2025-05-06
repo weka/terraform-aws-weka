3b7c28abf43f5d04a738b636e42b0ef18d7704bd
Assaf Giladi <assaf@weka.io>
2025-05-06 11:58:51 +0300

    docs: add example to tags_map variable description

--- variables.tf
+++ variables.tf
@@ -226,7 +226,7 @@ variable "install_weka_url" {
 variable "tags_map" {
   type        = map(string)
   default     = {}
-  description = "A map of tags to assign the same metadata to all resources in the environment. Format: key:value. Note: Manually tagged resources will be overridden by Terraform apply."
+  description = "A map of tags to assign the same metadata to all resources in the environment. Format: key:value. Note: Manually tagged resources will be overridden by Terraform apply. Example: if we want to have 2 tags, one with key \"Key1\" and value \"Val1\" and another one with key \"Key2\" and value \"Val2\" , we should set: tags_map = { \"Key1\" = \"Val1\", \"Key2\" = \"Val2\" }"
 }
 
 variable "set_dedicated_fe_container" {
