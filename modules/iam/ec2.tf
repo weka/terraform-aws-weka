e1ca8d1886f7b1b156a2b093b7e3bc8703f2b298
Denise Perez <densie@weka.io>
2025-12-17 09:30:27 +0200

    chore: add ec2:CreateTags to backend_eni_iam_policy

--- modules/iam/ec2.tf
+++ modules/iam/ec2.tf
@@ -18,6 +18,7 @@ resource "aws_iam_policy" "backend_eni_iam_policy" {
           "ec2:CreateNetworkInterface",
           "ec2:ModifyNetworkInterfaceAttribute",
           "ec2:DeleteNetworkInterface",
+          "ec2:CreateTags",
         ]
         Resource = "*"
       }
