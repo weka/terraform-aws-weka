387060276bbe732df676bda21c659fbb8f805647
Assaf Giladi <assaf@weka.io>
2026-02-03 15:03:29 +0200

    fix: return previous lambda log group and set lambda log group explicitly

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -6,9 +6,10 @@ locals {
       aws-apn-id = "pc:epkj0ftddjwa38m3oq9umjjlm"
     }
   )
-  lambda_prefix = lookup(var.custom_prefix, "lambda", var.prefix)
-  iam_prefix    = lookup(var.custom_prefix, "iam", var.prefix)
-  ec2_prefix    = lookup(var.custom_prefix, "ec2", var.prefix)
-  sfn_prefix    = lookup(var.custom_prefix, "sfn", var.prefix)
-  obs_prefix    = lookup(var.custom_prefix, "obs", var.prefix)
+  lambda_prefix     = lookup(var.custom_prefix, "lambda", var.prefix)
+  cloudwatch_prefix = lookup(var.custom_prefix, "cloudwatch", var.prefix)
+  iam_prefix        = lookup(var.custom_prefix, "iam", var.prefix)
+  ec2_prefix        = lookup(var.custom_prefix, "ec2", var.prefix)
+  sfn_prefix        = lookup(var.custom_prefix, "sfn", var.prefix)
+  obs_prefix        = lookup(var.custom_prefix, "obs", var.prefix)
 }
