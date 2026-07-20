5b001310ac8ba06569e09d06b778adce2ed7ff62
Assaf Giladi <assaf@weka.io>
2026-07-20 10:04:12 +0300

    fix: protocol gws containers parasing

--- modules/protocol_gateways/protocol_setup.sh
+++ modules/protocol_gateways/protocol_setup.sh
@@ -1,28 +1,22 @@
-# getting gw instance ips
+# wait until all protocol-gateway frontend containers are UP
 cluster_size="${gateways_number}"
-gw_ips_list=()
-instance_list=$(weka cluster container -J  | jq '.[] | "\(.host_ip) \(.cloud.instance_id)"' |uniq |tr '\n' ',')
-IFS="," read -ra ids <<< "$instance_list"
-for instance in "$${ids[@]}"; do
-  id=$(echo $instance | cut -d' ' -f2 | tr -d '"')
-  echo "$(date -u): get tags for instance $id"
-  if [[ -n `aws ec2 describe-tags --filters Name=resource-id,Values="$id" --region ${region} --output text | cut -f5 | grep ${gateways_name}` ]]; then
-	  ip=$(echo $instance | cut -d' ' -f1 | tr -d '"')
-    gw_ips_list+=($ip)
-  fi
-done
 
-gw_ips=""
-for ip in $${gw_ips_list[@]}; do
-  gw_ips+="-e $ip "
-done
+# gateway instance IDs (single AWS call, matched by the Name tag the module assigns)
+gw_instance_ids=($(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=${gateways_name}" \
+  --region ${region} \
+  --query 'Reservations[].Instances[].InstanceId' --output text))
 
 max_retries=60
 for (( retry=1; retry<=max_retries; retry++ )); do
-    # get all UP gateway container ids
-    all_container_ids=$(weka cluster container | grep frontend0 | grep -w -F $gw_ips | grep UP | awk '{print $1}')
-    # if number of all_container_ids < cluster_size, do nothing
-    all_container_ids_number=$(echo "$all_container_ids" | wc -l)
+    # UP frontend containers running on the gateway instances
+    all_container_ids=$(weka cluster container -J | jq -r '
+        .[]
+        | select(.name == "frontend0" and .status == "UP"
+                 and (.cloud.instance_id | IN($ARGS.positional[])))
+        | .id' --args "$${gw_instance_ids[@]}")
+
+    all_container_ids_number=$(echo "$all_container_ids" | grep -c .)
     if (( all_container_ids_number < cluster_size )); then
         echo "$(date -u): not all containers are ready - do retry $retry of $max_retries"
         sleep 20
